e16d79849722dbe2d986366035bb1bf071ec5918
knowhow <john@zenzic.com>
2015-05-13 10:45:13 -0700

    moved to a client model for easier event management

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 
 
-//server API
-exports.khRepository = require('./kh-api/kh-repository');
-exports.khJob = require('./kh-api/kh-job');
-exports.khAgent = require('./kh-api/kh-agent');
-exports.khWorkflow = require('./kh-api/kh-workflow');
-exports.khEventsHandler = require('./kh-api/kh-events-handler');
-//exports.commandTool = require('./kh-api/commandTool');
+/**
+ * Factory method for a KHClieent
+ * This is a programatic representation of a knowhow server.  It provides the same functionality that the knowhow
+ * server UI does.
+ *
+ * @param serverURL - the knwohow server being manipulated
+ */
+function KHClient(serverURL) {
+	var self = this;
+	console.log(serverURL);
+	self.serverURL = serverURL;
+	self.khEventHandler = new require('./kh-api/kh-events-handler')(serverURL);
+	self.khRepository = require('./kh-api/kh-repository')(serverURL, self.khEventHandler);
+	self.khJob = require('./kh-api/kh-job')(serverURL, self.khEventHandler);
+	self.khAgent = new require('./kh-api/kh-agent')(serverURL, self.khEventHandler);
+	self.khWorkflow = require('./kh-api/kh-workflow');
+	
+	self.end = function() {
+		console.log("ending KH client to: "+serverURL);
+		self.khEventHandler.removeAllListeners();
+		self.khEventHandler.close();
+		self.khEventHandler.disconnect();
+	}
+	return self;
+	
+}
+
+module.exports = KHClient;
