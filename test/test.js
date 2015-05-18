26341c4fe5def7470fda9681c6e446a7a46e1dbc
knowhow <john@zenzic.com>
2015-05-18 10:53:58 -0700

    updated test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,5 @@
 
-//create and start a knowhow-server
-var npm = require("npm");
-npm.load({}, function (er) {
-  if (er) return handlError(er)
-  npm.commands.install(["knowhow-server"], function (er, data) {
-    if (er) return commandFailed(er)
-    require('knowhow-server/server.js')(3001, function(err) {
+require('knowhow-server/server.js')(3001, function(err) {
 		console.log("server started");
 		var khClient = require("../index.js")("http://localhost:3001");
 
@@ -19,13 +13,7 @@ npm.load({}, function (er) {
 	  	console.log(repos);
 	  	process.exit(0);
 	});
-	});
+});
     
-  })
-  npm.registry.log.on("log", function (message) { 
-  	//console.log(message); 
-  });
-})
-
 
 
